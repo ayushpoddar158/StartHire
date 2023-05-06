4aa495c7f6a0827fc97ac5456a25e403a31ca335
TRY-ER <avyarth00@gmail.com>
2023-05-06 14:37:10 +0530

    modified:   src/App.jsx 	new file:   src/AppRoutes.jsx 	modified:   src/DashboardArea/Jobs.jsx 	modified:   src/main.jsx
    configuring routes for data transmission

--- src/App.jsx
+++ src/App.jsx
@@ -4,99 +4,68 @@ import { useState } from "react";
 
 //component
 import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
-import {Route,Routes} from "react-router-dom"
 import Footer from "./Components/Footer";
-import Signupstartup from "./Components/signupstartup"
-import LoginStartUp from "./Components/LoginStartUp";
-import Studentprofileform from "./Components/Studentprofileform";
-import Studentprofile from "./StudentDashboard/Studentprofile"
-import Filteredstudentlist from "./Components/Filteredstudentlist";
-import Forgetpassword from "./Components/Forgetpassword";
-import ChangePassword from "./Components/ChangePassword";
-import VerifyEmail  from './Components/VerifyEmail'
+import AppRoutes from "./AppRoutes";
 
-//jobs
-import CreateJobs from "./Components/jobs/CreateJobs";
-
-//components
-import  Dashboard  from './DashboardArea/Dashboard';
-import Aside from './DashboardArea/Aside'
-import StudentLists from "./DashboardArea/StudentLists";
-import SearchInterns from "./DashboardArea/SearchInterns";
-import StartUpprofileForm from "./DashboardArea/StartUpProfileForm";
-import Notification from './DashboardArea/Notification'
-import StartUpProfile from './DashboardArea/StartUpProfile'
-import StudentAside  from './StudentDashboard/StudentAside'
-import StudentDashboard  from './StudentDashboard/StudentDashboard'
-import UpdateJobs from "./Components/jobs/UpdateJobs";
 
 // Authorizer 
-import { AuthProvider } from "./Authorizer";
-import Jobs from "./DashboardArea/Jobs";
-import StartupBlog from "./DashboardArea/StartupBlog";
+import { AuthContext, AuthProvider } from "./Authorizer";
+import { useContext } from "react";
+import { useEffect } from "react";
+
+// Data setup 
+import { db } from "./Firebase";
+import {
+  query,
+  getDocs,
+  collection,
+  addDoc,
+  where
+} from "firebase/firestore";
+import { TryRounded } from "@mui/icons-material";
 
 const App = () => {
+  const { currentUser } = useContext(AuthContext)
+  const [id, setId] = useState(null);
+  const [isVerified, setIsVerified] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [isStartUp, setIsStartUp] = useState(false);
+  const [isStudent, setIsStudent] = useState(false);
 
-  // const getLocalItems=()=>{
-  //   const list=localStorage.getItem("lists")
-  //   if(list){
-  //     return JSON.parse(localStorage.getItem('lists'))
-  //   }
-  //   else{
-  //     return []
-  //   }
-  // }
-
-  // const [items,SetItems]=useState(getLocalItems())
-
-
-
-const [loginIsTrue,setLoginIsTrue]=useState(false)
-const [userName,setUserName]=useState("")
-const [generateotp,setGenerateotp]=useState("")
-const [uniqueId,setUniqueId]=useState("")
+  useEffect(() => {
+    const getUserData = async (currentUser) => {
+      console.log("inside get user data")
+      let id = await currentUser?.uid;
+      let isVerified = await currentUser?.emailVerified;
+      setId(id)
+      setIsVerified(isVerified);
+      try {
+        const q = query(collection(db, "startups"), where("uid", "==", id));
+        const docs = await getDocs(q);
+        if (docs.docs.length > 0) {
+          setIsStartUp(true);
+          setUserData(docs.docs[0].data());
+        }
+      }
+      catch (err) {
+        const q = query(collection(db, "users"), where("uid", "==", id));
+        const docs = await getDocs(q);
+        if (docs.docs.length > 0) {
+          setIsStudent(true);
+          setUserData(docs.docs[0].data());
+        }
+      }
+    }
+    if (currentUser) {
+      getUserData(currentUser);
+    }
+  }, [currentUser])
 
   return (
     <>
-    <AuthProvider>
-        <Navbar />
-        {/* <Home/> */}
-        <Routes>
-          <Route path="/"  element={<Home  userName={userName}  loginIsTrue={loginIsTrue}/>}/>
-          <Route path="/Home"  element={<Home  userName={userName}  loginIsTrue={loginIsTrue}/>}/>
-          <Route path="About" element={<About/>}/>
-          <Route path="Contact" element={<Contact/>}/>
-          <Route path="Login" element={<Login   setUserName={setUserName} loginIsTrue={loginIsTrue}  userName={userName}  setLoginIsTrue={setLoginIsTrue}/>}/>
-          <Route path="LoginStartUp" element={<LoginStartUp/>}/>
-          <Route path="Signup"  element={<Signup/>}/>
-          <Route path="Signupstartup" element={<Signupstartup/>}/>
-          <Route path="studentprofileform" element={<Studentprofileform/>}/>
-          <Route path="Studentprofile" element={<Studentprofile/>}/>
-          <Route path="Filteredstudentlist" element={<Filteredstudentlist/>}/>
-          <Route path="Forgetpassword" element={<Forgetpassword  uniqueId={uniqueId} setUniqueId ={setUniqueId}  generateotp={generateotp} setGenerateotp={setGenerateotp}/>}/>
-      
-          <Route path="ChangePassword" element={<ChangePassword  uniqueId={uniqueId} setUniqueId ={setUniqueId} />}/>
-          <Route path="Dashboard" element={<Dashboard />}/>
-          <Route path="StartUpprofileForm" element={<StartUpprofileForm />}/>
-          <Route path="StartUpProfile" element={<StartUpProfile />}/>
-          <Route path="VerifyEmail" element={<VerifyEmail/>}/>
-          <Route path="StudentAside" element={<StudentAside/>}/>
-          <Route path="StudentDashboard" element={<StudentDashboard/>}/>
-      
-
-
-          {/* //jobs routes */}
-          <Route path="CreateJobs" element={<CreateJobs/>}/>
-          <Route path="UpdateJobs/:id" element={<UpdateJobs/>}/>
-
-        </Routes>
-        <Footer/>
-      </AuthProvider>
+      <Navbar />
+      <AppRoutes userData={userData} isStartUp={isStartUp} isStudent={isStudent} isVerified={isVerified} />
+      <Footer />
     </>
   );
 };
---
+++ src/AppRoutes.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useState, Suspense } from "react";
+
+
+//component
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { Route, Routes } from "react-router-dom"
+import Signupstartup from "./Components/signupstartup"
+import LoginStartUp from "./Components/LoginStartUp";
+import Studentprofileform from "./Components/Studentprofileform";
+import Studentprofile from "./StudentDashboard/Studentprofile"
+import Filteredstudentlist from "./Components/Filteredstudentlist";
+import Forgetpassword from "./Components/Forgetpassword";
+import ChangePassword from "./Components/ChangePassword";
+import VerifyEmail from './Components/VerifyEmail'
+
+//jobs
+import CreateJobs from "./Components/jobs/CreateJobs";
+
+//components
+import Dashboard from './DashboardArea/Dashboard';
+import Aside from './DashboardArea/Aside'
+import StudentLists from "./DashboardArea/StudentLists";
+import SearchInterns from "./DashboardArea/SearchInterns";
+import StartUpprofileForm from "./DashboardArea/StartUpProfileForm";
+import Notification from './DashboardArea/Notification'
+import StartUpProfile from './DashboardArea/StartUpProfile'
+import StudentAside from './StudentDashboard/StudentAside'
+import StudentDashboard from './StudentDashboard/StudentDashboard'
+import UpdateJobs from "./Components/jobs/UpdateJobs";
+
+// Authorizer 
+import { AuthProvider } from "./Authorizer";
+import Jobs from "./DashboardArea/Jobs";
+import StartupBlog from "./DashboardArea/StartupBlog";
+
+const AppRoutes = (props) => {
+    let userData = props.userData;
+    const [loginIsTrue, setLoginIsTrue] = useState(false)
+    const [userName, setUserName] = useState("")
+    const [generateotp, setGenerateotp] = useState("")
+    const [uniqueId, setUniqueId] = useState("")
+
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home userName={userName} loginIsTrue={loginIsTrue} />} />
+                    <Route path="/Home" element={<Home userName={userName} loginIsTrue={loginIsTrue} />} />
+                    <Route path="About" element={<About />} />
+                    <Route path="Contact" element={<Contact />} />
+                    <Route path="Login" element={<Login setUserName={setUserName} loginIsTrue={loginIsTrue} userName={userName} setLoginIsTrue={setLoginIsTrue} />} />
+                    <Route path="LoginStartUp" element={<LoginStartUp />} />
+                    <Route path="Signup" element={<Signup />} />
+                    <Route path="Signupstartup" element={<Signupstartup />} />
+                    <Route path="studentprofileform" element={<Studentprofileform />} />
+                    <Route path="Studentprofile" element={<Studentprofile />} />
+                    <Route path="Filteredstudentlist" element={<Filteredstudentlist />} />
+                    <Route path="Forgetpassword" element={<Forgetpassword uniqueId={uniqueId} setUniqueId={setUniqueId} generateotp={generateotp} setGenerateotp={setGenerateotp} />} />
+
+                    <Route path="ChangePassword" element={<ChangePassword uniqueId={uniqueId} setUniqueId={setUniqueId} />} />
+                    <Route path="Dashboard" element={<Dashboard />} />
+                    <Route path="StartUpprofileForm" element={<StartUpprofileForm />} />
+                    <Route path="StartUpProfile" element={<StartUpProfile />} />
+                    <Route path="VerifyEmail" element={<VerifyEmail />} />
+                    <Route path="StudentAside" element={<StudentAside />} />
+                    <Route path="StudentDashboard" element={<StudentDashboard />} />
+
+
+
+                    {/* //jobs routes */}
+                    <Route path="CreateJobs" element={<CreateJobs />} />
+                    <Route path="UpdateJobs/:id" element={<UpdateJobs />} />
+
+                </Routes>
+            </Suspense>
+        </>
+    );
+};
+
+export default AppRoutes;
+
+
