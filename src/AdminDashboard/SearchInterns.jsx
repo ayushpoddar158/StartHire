6fa1bf25ab8651ef14c59757878e744e224afc80
TRY-ER <avyarth00@gmail.com>
2023-05-09 18:02:03 +0530

    modified:   src/AdminDashboard/SearchInterns.jsx 	modified:   src/App.jsx 	modified:   src/AppRoutes.jsx 	modified:   src/Components/Forgetpassword.jsx 	modified:   src/Components/Login.jsx 	modified:   src/Components/LoginStartUp.jsx
    setup the redirects accordingly for the admin panel

--- src/AdminDashboard/SearchInterns.jsx
+++ src/AdminDashboard/SearchInterns.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../style/Dashboard/SearchInterns.css";
 import Aside from '../DashboardArea/Aside'
-const SearchInterns = () => {
+const AdminInterns = () => {
   return (
     <>
       <div>
@@ -40,4 +40,4 @@ const SearchInterns = () => {
   );
 };
 
-export default SearchInterns;
+export default AdminInterns;
