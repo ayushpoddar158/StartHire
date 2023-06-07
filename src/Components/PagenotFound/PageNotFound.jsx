5ea36040eac71d84f21a64bfc742ae4db2ab69fa
Ayush Poddar <ayushpoddar158@gmail.com>
2023-06-07 11:54:06 +0530

    modified:   src/Components/Model/Model.jsx 	modified:   src/Components/PagenotFound/PageNotFound.css 	modified:   src/Components/PagenotFound/PageNotFound.jsx 	modified:   src/DashboardArea/JobDescp.jsx 	modified:   src/DashboardArea/css/JobDescp.css 	new file:   src/assets/404img1.webp 	modified:   src/const.css
    ayush 404 page created

--- src/Components/PagenotFound/PageNotFound.jsx
+++ src/Components/PagenotFound/PageNotFound.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-
+import Home from '../Home';
 import './PageNotFound.css';
-
+import pagenotfounfimg1 from '../../assets/404img1.webp';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+<div className="main404">
+
+  <div className="innerright404">
+  <h1 className='e404h1'>404 Error</h1>
+    <h1>That page dosent exist!</h1>
+    <p>Sorry the page you are looking for could not be fouund.</p>
+  </div>
+  <div className="buttonsection">
+    <button><Link className='page404Link' to='/Home'>Home page</Link></button>
+  </div>
+</div>
   )
 }
 
